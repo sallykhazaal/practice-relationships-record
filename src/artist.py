c198422fcfa959e190fa612234f5d765dcca118f
sallykhazaal <sallykhazaal@yahoo.com>
2023-03-30 10:12:49 -0400

    Completed Records Challenge

--- src/artist.py
+++ src/artist.py
@@ -1,2 +1,21 @@
+from src.record import Record
+
 class Artist:
-    pass
+    def __init__(self, name):
+        self.name = name 
+        self.records = []
+
+    def get_name(self):
+        return self.name
+    
+    def get_records(self):
+        return self.records
+    
+    def get_first_record(self):
+        earliest_record = self.records[0]
+
+        for record in self.records:
+            if record.year < earliest_record.year:
+                earliest_record = record
+        
+        return earliest_record
--- src/record.py
+++ src/record.py
@@ -1,2 +1,38 @@
 class Record:
-    pass
+
+    def __init__(self, title, artist, year):
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.songs = []
+        
+    def get_title(self):
+        return self.title.title()
+    
+    def get_year(self):
+        return self.year
+    
+    def get_artist(self):
+        return self.artist
+    
+    def get_songs(self):
+        return self.songs
+    
+    def total_runtime(self):
+        total_runtime = 0
+
+        for song in self.songs:
+            total_runtime += song.runtime
+        
+        return total_runtime
+    
+    def has_song(self, song):
+        return song in self.songs
+
+    def get_longest_song(self):
+        longest_runtime = self.songs[0]
+        for song in self.songs:
+            if song.runtime > longest_runtime.runtime:
+                longest_runtime = song
+
+        return longest_runtime
